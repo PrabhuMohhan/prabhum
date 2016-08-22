72322bfe3d0a5ef91d1942d675690132594df3ad
PrabhuMohhan <prabu751996@gmail.com>
2016-08-22 15:59:03 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,20 @@
+
+public class Positive 
+{
+public static void main(String[]args)
+{
+	int a=534;
+	if(a>0)
+{
+System.out.println("positive");	
+}
+else if(a<0)
+{
+	System.out.println("Negative");
+}
+else
+{
+	System.out.println("Zero");
+}
+}
+}
