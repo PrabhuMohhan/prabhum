42a7741de9002f76b99f4152a51a2c6b4f61c78e
PrabhuMohhan <prabu751996@gmail.com>
2016-08-22 14:38:22 +0530

    Add files via upload

---
+++ Activity01.java
@@ -0,0 +1,15 @@
+package MSPractice01;
+
+public class Activity01 
+	{
+	public static void main(String[]args)
+	{
+		int n = 5;
+	    int result = 1;
+	    for (int i = 1; i <= n; i++) {
+	      result = result * i;
+	    }
+	    System.out.println("The factorial of 5 is " + result);
+	  }
+	}
+ 
