17564c0e3fbb754a2532dc7f833b3d8269b11e9d
PrabhuMohhan <prabu751996@gmail.com>
2016-08-23 11:00:12 +0530

    Add files via upload

---
+++ Cameel.java
@@ -0,0 +1,18 @@
+
+public class Cameel {
+
+
+public static void main(String[] args) {
+    String start = "THIS_IS_A_TEST";
+    StringBuffer sb = new StringBuffer();
+    for (String s : start.split("_")) {
+        sb.append(Character.toLowerCase(s.charAt(0)));
+        if (s.length() > 1) {
+            sb.append(s.substring(1, s.length()).toLowerCase());
+        }
+    }
+    System.out.println(sb);
+}
+
+
+}
