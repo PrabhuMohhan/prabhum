d47f18db2a023836c5cf61caa9e49d039fba8f0d
PrabhuMohhan <prabu751996@gmail.com>
2016-08-23 13:36:44 +0530

    Add files via upload

---
+++ stringInt.java
@@ -0,0 +1,10 @@
+
+public class stringInt 
+{
+	public static void main(String[]args)
+	{
+	String Str = "1234";
+	int number = Integer.parseInt(Str);
+	System.out.println("The number is: " + number);
+}
+}
